6f5493b7d67dc4f120a621c2cff35096df0a15b0
jpablofdez <jpablofdez@localhost>
2021-11-03 16:33:58 -0600

    update

---
+++ src/main/resources/static/precache-manifest.0a542c131b467f28043fea6a72e253b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d42dc81b0d56694a70a",
+    "url": "/static/css/main.b5169cf4.chunk.css"
+  },
+  {
+    "revision": "9d42dc81b0d56694a70a",
+    "url": "/static/js/main.71cec8b5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3a1c05cfb503183e7b57",
+    "url": "/static/css/2.1e1eb2b6.chunk.css"
+  },
+  {
+    "revision": "3a1c05cfb503183e7b57",
+    "url": "/static/js/2.f88496fc.chunk.js"
+  },
+  {
+    "revision": "300d3423316bfd8f78937965d8374966",
+    "url": "/index.html"
+  }
+];
