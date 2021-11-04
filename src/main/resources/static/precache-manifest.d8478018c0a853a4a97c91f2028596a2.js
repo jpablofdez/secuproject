4edbdc72d1660867942c7f19b251f7690577f9df
jpablofdez <jpablofdez@localhost>
2021-11-03 18:15:36 -0600

    add static

---
+++ src/main/resources/static/precache-manifest.d8478018c0a853a4a97c91f2028596a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "672f1965fe6f54e61ade",
+    "url": "/static/css/main.b5169cf4.chunk.css"
+  },
+  {
+    "revision": "672f1965fe6f54e61ade",
+    "url": "/static/js/main.58cf1b4c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6a1f14dc9e3e8d8de88",
+    "url": "/static/css/2.1e1eb2b6.chunk.css"
+  },
+  {
+    "revision": "f6a1f14dc9e3e8d8de88",
+    "url": "/static/js/2.e26ea074.chunk.js"
+  },
+  {
+    "revision": "f1119c4ff47699209c58ac0bd67eb23f",
+    "url": "/index.html"
+  }
+];
